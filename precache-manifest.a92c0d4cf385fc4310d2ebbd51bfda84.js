8ffdaa383e5984b4daa2d7fb73b13f7ddce7c115
Dmitriy Kondratenko <dmitriy.kondratenko@nextiva.com>
2019-06-06 18:53:29 +0300

    Updates

---
+++ precache-manifest.a92c0d4cf385fc4310d2ebbd51bfda84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c419a8983f5434ddbf7",
+    "url": "/questions/static/js/runtime~main.54684bfb.js"
+  },
+  {
+    "revision": "e9171f42572f08d5da32",
+    "url": "/questions/static/js/main.a168a65d.chunk.js"
+  },
+  {
+    "revision": "7124fb0d490669a189b2",
+    "url": "/questions/static/js/2.67a28f79.chunk.js"
+  },
+  {
+    "revision": "e9171f42572f08d5da32",
+    "url": "/questions/static/css/main.e5d28b93.chunk.css"
+  },
+  {
+    "revision": "814cc801cf24a3881f424b6d78ee57ef",
+    "url": "/questions/index.html"
+  }
+];
